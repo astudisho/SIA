72e25ddce63564bef835e22a47925a7d9e75b9d0
Javier Astudillo <astudillo@outlook.com>
2016-10-14 01:07:17 -0500

    Colonia de hormigas incompleta

--- Practica05/colonia_hormigas.py
+++ Practica05/colonia_hormigas.py
@@ -1,80 +1,170 @@
-import random, math
+import random, math, numpy as np
+
+MIN = -5
+MAX = 5
+STEP = 10./16
 
 VALOR_INICIAL = 0
-NUMERO_HORMIGAS = NUMERO_CIUDADES = 16
+NUMERO_HORMIGAS = NUMERO_CIUDADES = 20
 
-GENERACIONES = 20
+MAX_GENERACIONES = 1
 ALFA = 1
 BETA = 5
 Q_DEPOSITO = 20
 P_EVAPORACION = 0.9
 PI_0 = 10e-6
 
+'''probabilidad = 	( ( feromonas_camino ^ importancia_feromona ) / ( distancia_camino ^ importancia_distancia ) ) / \
+				( ( suma_feromonas ^ importancia_feromona ) / ( suma_distancias ^ importancia_distancia ) ) '''
+
 class Camino(object):
 	"""docstring for Camino"""
-	def __init__(self, distancia, ):ff
+	def __init__(self, distancia ):
 		super(Camino, self).__init__()
-		self.__distancia = distancia
+		self.__distancia = abs(distancia)
 		self.__feromonas = PI_0
 
 	def __str__(self): return "Dst: " + str( round( self.__distancia , 3 ) ) + " Fer: " + str(self.__feromonas)
 
 	def getDistancia( self ):	return self.__distancia
-	def getFeromona( self ): return self.__feromoanas
+	def getFeromona( self ): return self.__feromonas
+	def evaporaFeromona( self , evaporacion = P_EVAPORACION ): self.__feromonas *= evaporacion
+	def depositaFeromona( self, deposito = Q_DEPOSITO ) : self.__feromonas += deposito
 
-class Ciudad(object):
-	"""docstring for Ciudad"""
-	def __init__(self, coordenadas, nombre ):
-		super(Ciudad, self).__init__()
-		self.__coordenadas = coordenadas
+class CaminosNodo(object):
+	"""docstring for CaminosNodo"""
+	def __init__(self, caminos):
+		super(CaminosNodo, self).__init__()
+
+		self.__sumaDistancias = 0
+		self.__sumaFeromonas = 0
+		self.__caminos = []
+
+		for i in caminos:
+			self.__caminos.append( Camino( i ) )
+
+		self.calculaSumaFeromonas()
+		self.calcularSumaDistancias()
+
+	def getCaminos(self): return self.__caminos
+
+	def __str__(self) : return 	"SumaDistancias: " + str(self.__sumaDistancias) + \
+								" SumaFeromonas: " +str(self.__sumaFeromonas)
+
+	def calculaSumaFeromonas(self):
+		self.__sumaFeromonas = 0
+
+		for camino in self.__caminos:
+			self.__sumaFeromonas += camino.getFeromona()
+
+	def calcularSumaDistancias(self):
+		self.__sumaDistancias = 0
+
+		for camino in self.__caminos:
+			self.__sumaDistancias += camino.getDistancia()
+
+	def evaporaFeromonas(self):
+		for camino in self.__caminos:
+			camino.evaporaFeromonas( P_EVAPORACION )
+
+	def ruleta( self ):
+		totalProbabilidad = 0
+
+		elegido = random.random()
+
+		print("Elegido " , elegido)
+		
+		for camino in self.__caminos:
+			print("Camino: " + str(camino) )
+			'''probabilidad =  ( ( camino.getFeromona() ** ALFA / camino.getDistancia() ** BETA ) )/ \
+							( ( self.__sumaFeromonas ** ALFA / self.__sumaDistancias ** BETA ) )'''
+
+			arriba = camino.getFeromona() ** ALFA / camino.getDistancia() ** BETA
+			abajo = self.__sumaFeromonas ** ALFA / self.__sumaDistancias ** BETA
+
+			print("Arriba = " , arriba )
+			print("Abajo = ", abajo )
+
+			print(arriba / float(abajo))
+
+			#print("probabilidad: " + str(probabilidad) )
+
+			totalProbabilidad += probabilidad
+
+			if elegido <= totalProbabilidad:
+				print( "Debug: " , totalProbabilidad )
+				return camino
+
+		print(len(self.poblacion))
+		return self.__caminos[ -1 ]
+		#print( elegido, totalProbabilidad )
+		raise
+		
+
+class Nodo(object):
+	"""docstring for Nodo"""
+	def __init__(self, valor, nombre ):
+		super(Nodo, self).__init__()
+		self.__valor = valor
 		self.__nombre = nombre
 
 	def __str__(self): return self.__nombre + " : " + str( self.__feromonas )
 
 	def getNombre(self):	return self.__nombre
-	def getCoordenada(self): return self.__coordenadas
-
+	def getCoordenada(self): return self.__valor
 
 class Hormiga():
-	def __init__(self, ciudadInicio):
-		self.__ciudadInicio = ciudadInicio
+	def __init__(self, nodoInicio):
+		self.__nodoInicio = nodoInicio
+		self.__distanciaRecorrida = 0
 		self.reset()
 
-	def reset():
-		self.__ciudadesVisitadas = [ ciudadInicio ]
+	def reset(self):
+		self.__nodosVisitados = [ self.__nodoInicio ]
 
-		self.__ciudadesPorVisitar = range( NUMERO_CIUDADES )
-		self.__ciudadesPorVisitar = self.__ciudadesPorVisitar.remove( ciudadInicio )
+		self.__nodosPorVisitar = range( NUMERO_CIUDADES )
+		self.__nodosPorVisitar = self.__nodosPorVisitar.remove( self.__nodoInicio )
+		self.__distanciaRecorrida = 0
+
+	def recorreNodos(self):
+		nodoActual = self.__nodoInicio
+
+		while( len(self.__nodosVisitados) == NUMERO_CIUDADES ):
+			caminoElegido = ColoniaHormigas.CAMINOS[ nodoActual ] . ruleta()
+
+			print(caminoElegido.getDistancia())
 
 
-CIUDADES = {	
-				0 :	Ciudad( (38.24, 20.42) , "Ciudad01" ),
-				1 : Ciudad( (39.57, 26.15) , "Ciudad02" ),
-				2 : Ciudad(	(40.56, 25.32) , "Ciudad03" ),
-				3 : Ciudad( (36.26, 03.12) , "Ciudad04" ),
+class ColoniaHormigas(object):
+	"""docstring for ColoniaHormigas"""
+	def __init__(self):
+		super(ColoniaHormigas, self).__init__()
+	
+	CAMINOS = []
+	HORMIGAS = []
+	#CAMINOS = [ ([  ] * NUMERO_CIUDADES) for x in range(NUMERO_CIUDADES) ]
+	for x in range(NUMERO_CIUDADES):
+		CAMINOS.append( CaminosNodo(np.arange(MIN, MAX, STEP) ) )
 
-				4 : Ciudad( (33.48, 10.54) , "Ciudad05" ),
-				5 : Ciudad(	(37.56, 12.19) , "Ciudad06" ),
-				6 : Ciudad( (38.42, 13.11) , "Ciudad07" ),
-				7 : Ciudad( (37.52, 20.44) , "Ciudad08" ),
+	def cicloPrincipal(self):
+		contadorGeneraciones = 0
 
-				8 : Ciudad( (41.23, 09.10) , "Ciudad09" ),
-				9 : Ciudad( (41.17, 09.10) , "Ciudad10" ),
-				10: Ciudad( (36.08, -5.21) , "Ciudad11" ),
-				11: Ciudad( (38.47, 15.13) , "Ciudad12" ),
+		while( contadorGeneraciones != MAX_GENERACIONES ):
 
-				12: Ciudad( (38.15, 15.35) , "Ciudad13" ),
-				13: Ciudad( (37.51, 15.17) , "Ciudad14" ),
-				14: Ciudad( (35.49, 14.32) , "Ciudad15" ),
-				15: Ciudad( (39.36, 19.56) , "Ciudad16" )
-			}
+			for hormiga in colonia.HORMIGAS:
+				pass
 
-def distancia(ciudadX, ciudadY):
-	return math.sqrt(	( ciudadX.getCoordenada()[ 0 ] - ciudadY.getCoordenada()[ 0 ] ) ** 2 + \
-						( ciudadX.getCoordenada()[ 1 ] - ciudadY.getCoordenada()[ 1 ] )	** 2 )
+			contadorGeneraciones += 1
 
-CAMINOS = [ ([  ] * NUMERO_CIUDADES) for x in range(NUMERO_CIUDADES) ]
+if __name__ == '__main__':
+	colonia = ColoniaHormigas()
+	hormiga = Hormiga(0)
 
-for indiceX, ciudadX in enumerate( CIUDADES ):
-	for indiceY, ciudadY in enumerate( CIUDADES ):
-		CAMINOS[ indiceX ].append( Camino( distancia( CIUDADES[ ciudadX ], CIUDADES[ ciudadY ] ) ) )
+	'''for i in ColoniaHormigas.CAMINOS:
+		print(i)'''
+	#ColoniaHormigas.CAMINOS[0].calcularSumaDistancias()
+	#ColoniaHormigas.CAMINOS[0].calculaSumaFeromonas()
+
+	print(ColoniaHormigas.CAMINOS[0])
+
+	print(ColoniaHormigas.CAMINOS[0].ruleta())
